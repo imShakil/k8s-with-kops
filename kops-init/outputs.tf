1673fadf5571a66081aff832b11ede3b077a3edc
Global Mobarak <mh.ice.iu@gmail.com>
2025-12-05 19:51:11 +0600

    fix: improved scripts

--- kops-init/outputs.tf
+++ kops-init/outputs.tf
@@ -1,10 +1,10 @@
 output "kops_init" {
   description = "Kops initialization infrastructure outputs"
   value = {
-    cluster_name = var.cluster_name
-    state_bucket = module.kops_state_store.bucket_name
-    iam_user_name = module.kops_iam.kops_admin_user_info.user_name
-    iam_user_arn = module.kops_iam.kops_admin_user_info.user_arn
+    cluster_name  = var.cluster_name
+    state_bucket  = module.kops_state_store.bucket_name
+    iam_user_name = module.iam.kops_admin_user_info.user_name
+    iam_user_arn  = module.iam.kops_admin_user_info.user_arn
   }
 }
 output "kops_admin_credentials" {
