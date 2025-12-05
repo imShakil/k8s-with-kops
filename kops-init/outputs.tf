765b67c3c8f1f5888b635d09613fb1dccfcc707c
Global Mobarak <mh.ice.iu@gmail.com>
2025-12-05 15:38:35 +0600

    fixed: s3 module output

--- kops-init/outputs.tf
+++ kops-init/outputs.tf
@@ -2,6 +2,6 @@ output "kops_init" {
   description = "Kops initialization infrastructure outputs"
   value = {
     cluster_name      = var.cluster_name
-    state_bucket      = module.s3.bucket_name
+    state_bucket      = module.kops_state_store.bucket_name
   }
 }
